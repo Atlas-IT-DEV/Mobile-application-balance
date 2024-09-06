77483f5a40b0cb19fc6d68a60a1b6005f5eb1412
Clourincess <96530726+Clourincess@users.noreply.github.com>
2024-09-06 16:05:02 +0300

    added modal delete account

--- pages/profile_screen/profile_screen.jsx
+++ pages/profile_screen/profile_screen.jsx
@@ -6,9 +6,13 @@ import {
   ScrollView,
   Text,
   TouchableOpacity,
+  Modal,
 } from "react-native";
 
 import SubscribeMiniCard from "../../components/subscribe_mini_card";
+import { useState } from "react";
+import { SvgXml } from "react-native-svg";
+import { closeIcon, warningIcon } from "../../images/images";
 
 const ProfileScreen = ({
   name = "Имя",
@@ -16,6 +20,7 @@ const ProfileScreen = ({
   phone_number = "+7 (800) 555 35-35",
   inn = "8888 888 888 88",
 }) => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" />
@@ -86,11 +91,49 @@ const ProfileScreen = ({
           <TouchableOpacity>
             <Text style={styles.actionButtonText}>Выйти из аккаунта</Text>
           </TouchableOpacity>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => setModalVisible(true)}>
             <Text style={styles.actionButtonText}>Удалить аккаунт</Text>
           </TouchableOpacity>
         </View>
       </ScrollView>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => setModalVisible(!modalVisible)}
+          >
+            <SvgXml xml={closeIcon} />
+          </TouchableOpacity>
+          <View style={styles.modalView}>
+            <SvgXml xml={warningIcon} />
+
+            <Text style={styles.warningText}>
+              Вы действительно хотите удалить аккаунт?
+            </Text>
+            <View style={styles.buttons}>
+              <TouchableOpacity
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.buttonCloseText}>Нет</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.button, styles.buttonDelete]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.buttonDeleteText}>Да</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
@@ -190,6 +233,69 @@ const styles = StyleSheet.create({
   actionButtonText: {
     fontFamily: "IBMMedium",
     color: "rgba(113, 113, 113, 1)",
+  },
+
+  centeredView: {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(52, 52, 52, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    marginHorizontal: 38,
+    backgroundColor: "white",
+    borderRadius: 20,
+    paddingVertical: 40,
+    paddingHorizontal: 30,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+  },
+  warningText: {
+    marginTop: 20,
+    color: "rgba(22, 3, 111, 1)",
+    fontFamily: "IBMSemiBold",
+    fontSize: 18,
+    textAlign: "center",
+  },
+  buttons: {
+    marginTop: 20,
+    gap: 15,
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    minWidth: "100%",
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    height: 40,
+  },
+  buttonDelete: {
+    borderWidth: 1,
+    borderEndColor: "rgba(22, 3, 111, 1)",
+  },
+  buttonClose: {
+    backgroundColor: "rgba(48, 31, 129, 1)",
+  },
+  buttonCloseText: {
+    color: "white",
+    fontFamily: "IBMSemiBold",
+  },
+  buttonDeleteText: {
+    color: "rgba(22, 3, 111, 1)",
+    fontFamily: "IBMMedium",
+  },
+  closeButton: {
+    position: "absolute",
+    top: "25%",
+    right: "7%",
   },
 });
 
